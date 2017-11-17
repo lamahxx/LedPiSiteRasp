1435713f57b1fb4fddd3c876859325c12163a2bf
lamahxx <lamahxx@users.noreply.github.com>
2017-11-17 20:01:55 +0100

    second commit

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <!DOCTYPE html>
 <html>
+<title>Franky Pi</title>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://googleapis.com/css?family=Lato">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<style>
+		body,h1,h2 {font-family : "Lato"; sans-serif}
+		p {line-height : 2}
+		.bgimg {background-image: url("https://i.imgur.com/4c8hBV0.jpg")}
+		</style>
+
+	<body>
+		<header class="w3-display-container w3-wide bgimg w3-grayscale-mine" id="home">
+			<div class="w3-display-middle w3-text-white w3-center">
+				<h1 class="w3-jumbo">Welcome Stranger</h1>
+				<h2><b>nothing here matters...</b></h2>
+			</div>
+		</header>
+
+		<footer class="w3-center w3-black w3-padding-16">
+			<p>Powered by Lamahxx</p>
+		</footer>
+	</body>
 </html>
